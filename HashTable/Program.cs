20acb690118ddcb9656cf8c7a9c774fe4d52dc15
Christopher Contreras <radicaly2k@gmail.com>
2019-11-03 19:41:41 -0800

    Added first commit of project

---
+++ HashTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hTable = new Hashtable();
+
+            hTable.Add("Test", 1);
+            hTable.Add("Test2", 10);
+            hTable.Add("Test3", 100);
+            hTable.Add("Test4", 1000);
+            hTable.Add("Test5", 10000);
+            hTable.Add("Test6", 20);
+
+            foreach(object obj in hTable.Values)
+            {
+                Console.WriteLine($"Content: {obj.ToString()}");
+            }
+        }
+    }
+}
